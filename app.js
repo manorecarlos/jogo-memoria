f5c1dd9f50f33028f4c2042d84874aea155d42fc
Renan Carlos <renancarlos3010@gmail.com>
2021-02-09 17:15:44 -0300

    Fim da segunda aula

--- app.js
+++ app.js
@@ -1,45 +1,63 @@
+// Selecionando o elemento HTML onde as cartas serão inseridas
 const board = document.querySelector(".table-board")
 
+// Pares de cartas iniciais
 let pairs = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 
+// Pares de cartas selecionadas
+let selectedCards = []
+
+// Criando as cartas no tabuleiro de acordo com o número de pares iniciais
 pairs.forEach(pair => {
     const card = document.createElement("div")
 
     card.className = "table-board-card"
+    card.dataset.value = pair
     card.innerText = pair
 
-    card.addEventListener("click", SameCards)
+    card.addEventListener("click", compareCards)
 
     board.append(card)
 })
 
-let value1
-let value2
+// Verifica se as cartas selecionadas são iguais ou diferentes
+function compareCards(value) {
+    const clickedCard = value.target
 
-function SameCards(card) {
-    if(value1 > 0) {
-        value2 = card.target.innerText
-    } else {
-        value1 = card.target.innerText
+    if(clickedCard.classList.contains('selected-card')) {
+        console.log("Esta carta já foi selecionada. Selecione outra carta")
+
+        return
     }
 
-    if(value1 == value2) {
-        console.log("Formou um par")
-        value1 = 0
-        value2 = 0
-    } else {
-        console.log("Não formou um par")
-        value1 = 0
-        value2 = 0
-    }
+    // Adiciona um estilo a carta clicada
+    clickedCard.classList.add('selected-card')
 
-    console.log("O primeiro valor é ", value1)
-    console.log("O segundo valor é ", value2)
+    // Adiciona a carta selecionada ao array
+    selectedCards.push(clickedCard)
+
+    if(selectedCards.length > 1) {
+        const [firstCard, secondCard] = [...selectedCards]
+
+        console.log("Primeira carta = " + firstCard.dataset.value)
+        console.log("Segunda carta = " + secondCard.dataset.value)
+
+        if(firstCard.dataset.value === secondCard.dataset.value) {
+            console.log("As cartas são iguais!")
+
+            // Limpa o array
+            selectedCards = []
+        } else {
+            console.log("As cartas não são iguais!")
+            
+            // Limpa o array
+            selectedCards = []
+
+            // Remove o estilo das cartas selecionadas após 1 segundo
+            setTimeout(() => {
+                firstCard.classList.remove('selected-card')
+                secondCard.classList.remove('selected-card')
+            },1000)
+        }
+    }
 }
-
-
-// 1h21
-
-
-
-
