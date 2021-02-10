6aa166a60fc4da0ca4fad0557d6cfeb584a94d09
Renan Carlos <renancarlos3010@gmail.com>
2021-02-10 18:49:46 -0300

    Fim 10/02

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 // Selecionando o elemento HTML onde as cartas serão inseridas
 const board = document.querySelector(".table-board")
 
+// Selecionado o elemento HTML onde a pontuação será inserida
+const score = document.querySelector(".table-score")
+
 // Pares de cartas iniciais
-let pairs = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+let initialPairs = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 
 // Pares de cartas selecionadas
 let selectedCards = []
 
+// Contador da pontuação
+let scoreCount = 0
+
+// Carregando o jogo
+const loadGame = () => {
+    scoreCount = 0
+    selectedCards = []
+
+    const shufflePairs = shuffle(initialPairs)
+
+    createCards(shufflePairs)
+
+    console.log(shufflePairs)
+}
+
+// Embaralhando as cartas
+const shuffle = (pairs) => {
+    for(let i = pairs.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [pairs[i], pairs[j]] = [pairs[j], pairs[i]]
+    }
+
+    return pairs
+}
+
 // Criando as cartas no tabuleiro de acordo com o número de pares iniciais
-pairs.forEach(pair => {
-    const card = document.createElement("div")
+const createCards = (pairs) => {
+    pairs.forEach(pair => {
+        const card = document.createElement("div")
 
-    card.className = "table-board-card"
-    card.dataset.value = pair
-    card.innerText = pair
+        card.className = "table-board-card"
+        card.dataset.value = pair
+        card.innerText = pair
 
-    card.addEventListener("click", compareCards)
+        card.addEventListener("click", compareCards)
 
-    board.append(card)
-})
+        board.append(card)
+    })
+}
 
 // Verifica se as cartas selecionadas são iguais ou diferentes
-function compareCards(value) {
+const compareCards = (value) => {
     const clickedCard = value.target
 
     if(clickedCard.classList.contains('selected-card')) {
@@ -31,7 +61,7 @@ function compareCards(value) {
     }
 
     // Adiciona um estilo a carta clicada
-    clickedCard.classList.add('selected-card')
+    clickedCard.classList.toggle('selected-card')
 
     // Adiciona a carta selecionada ao array
     selectedCards.push(clickedCard)
@@ -45,8 +75,15 @@ function compareCards(value) {
         if(firstCard.dataset.value === secondCard.dataset.value) {
             console.log("As cartas são iguais!")
 
+            // Quando as cartas forem iguais, aumenta a pontuação
+            scoreCount = scoreCount + 1
+
+            // Exibe a pontuação quando as cartas forem iguais
+            score.innerText = `Pontuação: ${scoreCount}`
+
             // Limpa o array
             selectedCards = []
+
         } else {
             console.log("As cartas não são iguais!")
             
@@ -55,9 +92,13 @@ function compareCards(value) {
 
             // Remove o estilo das cartas selecionadas após 1 segundo
             setTimeout(() => {
-                firstCard.classList.remove('selected-card')
-                secondCard.classList.remove('selected-card')
+                firstCard.classList.toggle('selected-card')
+                secondCard.classList.toggle('selected-card')
             },1000)
         }
     }
 }
+
+loadGame()
+
+//1h24
